3b664c85c59a5a5b0895091d52c6fce74afd2ec0
Israfil Hossen <sisrafilss@gmail.com>
2026-03-09 22:12:23 +0600

    deployment issue resolved

--- prisma/schema/migrations/20260116095918_payment/migration.sql
+++ prisma/schema/migrations/20260116095918_payment/migration.sql
@@ -1,5 +1,15 @@
 -- AlterTable
-ALTER TABLE "bookings" ADD COLUMN     "paymentId" TEXT;
+DO $$ 
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'bookings' AND column_name = 'paymentId') THEN
+        ALTER TABLE "bookings" ADD COLUMN "paymentId" TEXT;
+    END IF;
+END $$;
 
 -- AlterTable
-ALTER TABLE "payments" ADD COLUMN     "invoiceUrl" TEXT;
+DO $$ 
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'payments' AND column_name = 'invoiceUrl') THEN
+        ALTER TABLE "payments" ADD COLUMN "invoiceUrl" TEXT;
+    END IF;
+END $$;
